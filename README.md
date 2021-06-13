882376549e00cf87d1c5485c20236f088058ae8a
MichalBelko <michalbelko02@gmail.com>
2021-06-13 20:21:56 +0200

    Updates

--- README.md
+++
@@ -1,9 +0,0 @@
-# A clean beautiful and responsive Software Developer Portfolio ⚡️
-
-## Portfolio Sections
-
-✔️ Summary and About me
-✔️ Skills
-✔️ Big Projects
-✔️ Contact me
-✔️ Github Profile
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f2bb63fdeba6343927466eb7c1049d08.js"
+  "/Portfolio/precache-manifest.d0a81fa8cb2b8ee36e1d607f62156d3d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
